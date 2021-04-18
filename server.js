b1ea855f5601612e828c9a18b45833019de8200f
juanong <juanong26@gmail.com>
2021-04-18 17:50:21 -0400

    Added uri variable to server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // require is equivalent to import
 const express = require('express')
 const app = express()
+// Connect this to Atlas
+const uri = process.env.MONGODB_URI
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
